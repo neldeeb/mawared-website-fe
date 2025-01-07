c99f8ad7c127af4c90a37225d5e9b39ec7974a2f
neldeeb <neldeeb@qurtobasolutions.com>
2025-01-07 05:09:44 +0200

    add navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,24 +1,22 @@
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-} from "@/components/ui/navigation-menu";
+import { Button } from "@/components/ui/button";
 
 const Navbar = () => {
   return (
-    <NavigationMenu>
-      <NavigationMenuList>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <NavigationMenuLink>Link</NavigationMenuLink>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
+    <div className="bg-[#FFFFFFA6] flex items-center justify-between custom-nav-shadow-box-style px-24 h-20">
+      <img src="/img/mawared-logo.png" alt="logo" />
+
+      <div className="flex items-center justify-between gap-10">
+        <div className="font-medium text-xl text-[#313030]">Home</div>
+        <div className="font-medium text-xl text-[#313030]">Features</div>
+        <div className="font-medium text-xl text-[#313030]">FAQ</div>
+        <div className="font-medium text-xl text-[#313030]">Plans</div>
+        <div className="font-medium text-xl text-[#313030]">About</div>
+      </div>
+
+      <Button className="custom-nav-btn-style font-medium text-lg">
+        Book a live demo
+      </Button>
+    </div>
   );
 };
 
