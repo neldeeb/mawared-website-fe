bff9534d0f898c710472332eeb2ba6cc927b67aa
neldeeb <neldeeb@qurtobasolutions.com>
2025-01-12 05:12:39 +0200

    handle navigation menu

--- src/AppRoutes/index.tsx
+++ src/AppRoutes/index.tsx
@@ -1,11 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AboutUsPage from "@/containers/AboutUsPage";
+import HomePage from "@/containers/HomePage";
+import FeaturesPage from "@/containers/FeaturesPage";
+import FAQPage from "@/containers/FAQPage";
+import PlansPage from "@/containers/PlansPage";
+import ContactUsPage from "@/containers/ContactUsPage";
 
 const AppRoutes = () => {
   return (
     <Router>
       <Routes>
-        <Route path="*" element={<AboutUsPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/features" element={<FeaturesPage />} />
+        <Route path="/faq" element={<FAQPage />} />
+        <Route path="/plans" element={<PlansPage />} />
+        <Route path="/about" element={<AboutUsPage />} />
+        <Route path="/contact" element={<ContactUsPage />} />
       </Routes>
     </Router>
   );
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,21 +1,182 @@
 import { Button } from "@/components/ui/button";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { Menu } from "lucide-react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const currentRoute = useLocation();
+  const navigate = useNavigate();
+
+  // handle navigate to page function
+  const handleNavigateToPage = () => {
+    navigate(`/contact`);
+  };
+
   return (
-    <div className="bg-[#FFFFFFA6] flex items-center justify-between custom-nav-shadow-box-style px-24 h-20 fixed w-full">
+    <div className="bg-[#FFFFFFA6] flex items-center justify-between custom-nav-shadow-box-style gap-10 px-24 h-20 fixed w-full">
       <img src="/img/mawared-logo.png" alt="logo" />
 
-      <div className="flex items-center justify-between gap-10">
-        <div className="font-medium text-xl text-[#313030]">Home</div>
-        <div className="font-medium text-xl text-[#313030]">Features</div>
-        <div className="font-medium text-xl text-[#313030]">FAQ</div>
-        <div className="font-medium text-xl text-[#313030]">Plans</div>
-        <div className="font-medium text-xl text-[#313030]">About</div>
+      <div className="items-center justify-between gap-10 hidden lg:flex">
+        <Link to="/">
+          <div
+            className={
+              currentRoute?.pathname === "/"
+                ? "font-semibold text-xl text-customBlueWaveyColor"
+                : "font-medium text-xl text-[#313030]"
+            }
+          >
+            Home
+          </div>
+        </Link>
+        <Link to="/features">
+          <div
+            className={
+              currentRoute?.pathname === "/features"
+                ? "font-semibold text-xl text-customBlueWaveyColor"
+                : "font-medium text-xl text-[#313030]"
+            }
+          >
+            Features
+          </div>
+        </Link>
+        <Link to="/faq">
+          <div
+            className={
+              currentRoute?.pathname === "/faq"
+                ? "font-semibold text-xl text-customBlueWaveyColor"
+                : "font-medium text-xl text-[#313030]"
+            }
+          >
+            FAQ
+          </div>
+        </Link>
+        <Link to="/plans">
+          <div
+            className={
+              currentRoute?.pathname === "/plans"
+                ? "font-semibold text-xl text-customBlueWaveyColor"
+                : "font-medium text-xl text-[#313030]"
+            }
+          >
+            Plans
+          </div>
+        </Link>
+        <Link to="/about">
+          <div
+            className={
+              currentRoute?.pathname === "/about"
+                ? "font-semibold text-xl text-customBlueWaveyColor"
+                : "font-medium text-xl text-[#313030]"
+            }
+          >
+            About
+          </div>
+        </Link>
       </div>
 
-      <Button className="custom-nav-btn-style font-medium text-lg">
-        Book a live demo
-      </Button>
+      <div className="hidden lg:flex">
+        <Button
+          className="flex custom-nav-btn-style font-medium text-lg"
+          onClick={handleNavigateToPage}
+        >
+          Book a live demo
+        </Button>
+      </div>
+
+      {/* start of mobile navbar */}
+      <div className="flex lg:hidden">
+        <Drawer>
+          <DrawerTrigger>
+            <Menu color="#1997e1" size={38} />
+          </DrawerTrigger>
+
+          <DrawerContent>
+            <DrawerHeader className="flex justify-center items-center pt-11 sm:text-center">
+              <DrawerDescription>
+                <Link to="/">
+                  <div
+                    className={
+                      currentRoute?.pathname === "/"
+                        ? "font-semibold text-xl text-customBlueWaveyColor mb-3"
+                        : "font-medium text-xl text-[#313030] mb-3"
+                    }
+                  >
+                    Home
+                  </div>
+                </Link>
+                <Link to="/features">
+                  <div
+                    className={
+                      currentRoute?.pathname === "/features"
+                        ? "font-semibold text-xl text-customBlueWaveyColor mb-3"
+                        : "font-medium text-xl text-[#313030] mb-3"
+                    }
+                  >
+                    Features
+                  </div>
+                </Link>
+                <Link to="/faq">
+                  <div
+                    className={
+                      currentRoute?.pathname === "/faq"
+                        ? "font-semibold text-xl text-customBlueWaveyColor mb-3"
+                        : "font-medium text-xl text-[#313030] mb-3"
+                    }
+                  >
+                    FAQ
+                  </div>
+                </Link>
+                <Link to="/plans">
+                  <div
+                    className={
+                      currentRoute?.pathname === "/plans"
+                        ? "font-semibold text-xl text-customBlueWaveyColor mb-3"
+                        : "font-medium text-xl text-[#313030] mb-3"
+                    }
+                  >
+                    Plans
+                  </div>
+                </Link>
+                <Link to="/about">
+                  <div
+                    className={
+                      currentRoute?.pathname === "/about"
+                        ? "font-semibold text-xl text-customBlueWaveyColor mb-3"
+                        : "font-medium text-xl text-[#313030] mb-3"
+                    }
+                  >
+                    About
+                  </div>
+                </Link>
+              </DrawerDescription>
+            </DrawerHeader>
+
+            <DrawerFooter className="flex justify-center items-center gap-6">
+              <Button
+                className="custom-nav-btn-style font-medium text-lg w-1/2"
+                onClick={handleNavigateToPage}
+              >
+                Book a live demo
+              </Button>
+
+              <DrawerClose className="w-1/2">
+                <Button className="bg-[#313030] text-white font-medium text-lg border rounded-xl w-full py-5">
+                  Cancel
+                </Button>
+              </DrawerClose>
+            </DrawerFooter>
+          </DrawerContent>
+        </Drawer>
+      </div>
+      {/* end of mobile navbar */}
     </div>
   );
 };
