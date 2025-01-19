7f7cb4d9433dc302922aaffe79c43d2783b0b47c
neldeeb <neldeeb@qurtobasolutions.com>
2025-01-20 01:36:42 +0200

    add usa contact to footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -48,11 +48,11 @@ const Footer = () => {
         </div>
 
         {/* Contact info */}
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-5 text-sm font-normal py-12">
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-5 text-sm font-normal py-12">
           <div className="flex items-center gap-6 text-white">
             <img src="./img/location-icon.png" alt="location-icon" />
             24 Aziz Koheil st from Abdel Hamid AlDeeb St., Tharwat, Alexandria,
-            21599,Egypt.
+            21599, Egypt.
           </div>
 
           <div className="flex items-center gap-6 text-white">
@@ -67,24 +67,34 @@ const Footer = () => {
           </div>
 
           <div className="flex items-center gap-6 text-white">
+            <img src="./img/location-icon.png" alt="location-icon" />
+            257 Maynard Summit Way, Cary, NC 27511, USA.
+          </div>
+
+          <div className="flex items-center gap-6 text-white">
             <img src="./img/call-icon.png" alt="call-icon" />
             (+20) 03 5849945
           </div>
 
           <div className="flex items-center gap-6 text-white">
-            <img src="./img/email-icon.png" alt="email-icon" />
-            info@mawaredhr.com
+            <img src="./img/call-icon.png" alt="call-icon" />
+            (+20) 10 25285760
           </div>
 
           <div className="flex items-center gap-6 text-white">
             <img src="./img/call-icon.png" alt="call-icon" />
             (+971) 50 460 2733
           </div>
+
+          <div className="flex items-center gap-6 text-white">
+            <img src="./img/email-icon.png" alt="email-icon" />
+            info@mawaredhr.com
+          </div>
         </div>
       </div>
 
       {/* Bottom section */}
-      <div className="flex flex-col md:flex-row justify-between items-center bg-[#00000033] px-24 gap-0 md:gap-10">
+      <div className="flex flex-col md:flex-row justify-between items-center text-center md:text-left bg-[#00000033] px-24 gap-0 md:gap-10">
         <p className="text-white text-sm font-normal my-4">
           All rights reserved for MAWARED HR {new Date().getFullYear()} ©
         </p>
