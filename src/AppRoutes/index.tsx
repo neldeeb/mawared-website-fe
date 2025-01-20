7398ff0b32d9c6773dbb753157b801b6ca30e1dc
neldeeb <neldeeb@qurtobasolutions.com>
2025-01-20 12:11:58 +0200

    hide plans page

--- src/AppRoutes/index.tsx
+++ src/AppRoutes/index.tsx
@@ -3,7 +3,7 @@ import AboutUsPage from "@/containers/AboutUsPage";
 import HomePage from "@/containers/HomePage";
 // import FeaturesPage from "@/containers/FeaturesPage";
 import FAQPage from "@/containers/FAQPage";
-import PlansPage from "@/containers/PlansPage";
+// import PlansPage from "@/containers/PlansPage";
 import ContactUsPage from "@/containers/ContactUsPage";
 import { Toaster } from "react-hot-toast";
 
@@ -15,7 +15,7 @@ const AppRoutes = () => {
           <Route path="/" element={<HomePage />} />
           {/* <Route path="/features" element={<FeaturesPage />} /> */}
           <Route path="/faq" element={<FAQPage />} />
-          <Route path="/plans" element={<PlansPage />} />
+          {/* <Route path="/plans" element={<PlansPage />} /> */}
           <Route path="/about" element={<AboutUsPage />} />
           <Route path="/contact" element={<ContactUsPage />} />
         </Routes>
